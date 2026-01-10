486f15dbbfdfa44742956d10bc85e8e5b54604e5
Shiva Deshpande <deshps11@mcmaster.ca>
2026-01-10 17:31:01 -0500

    Layout and basic functionality

--- web/app/admin/page.js
+++ web/app/admin/page.js
@@ -1,19 +1,65 @@
 "use client";
+import { useState } from "react";
 
 export default function Admin() {
-    let db = [
+    const [db, setDb] = useState([
     { LeBron: [{ Tylenol: 1 }, { Vivace: 3 }] },
     { LeTwo: [{ Tylenol: 4 }, { Vivace: 2 }] },
-    ];
+    ]);
+
+    const incrementDosage = () => {
+        setDb(prev => {
+            const copy = structuredClone(prev)
+            copy[0].LeBron[0].Tylenol += 1
+            return copy
+        } 
+        );
+    }
+    const decrementDosage = () => {
+        setDb(prev => {
+            const copy = structuredClone(prev)
+            copy[0].LeBron[0].Tylenol -= 1
+            return copy
+        }
+        );
+    }
 
     return (
-        <div>
+        <div className="p-10">
             <title>
                 Admin Page
             </title>
-            <h1 className="flex flex-col align-left font-size 30">
+            <h1 className="flex justify-left text-3xl">
                 Add a prescription to get started.
             </h1>
+            <table className="w-9/12 border border-gray-300 m-10 mx-auto">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2 border">User</th>
+                        <th className="px-4 py-2 border">Medication</th>
+                        <th className="px-4 py-2 border">Dosage</th>
+                        <th className="px-4 py-2 border">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="px-4 py-2 border text-center">{Object.keys(db[0])[0]}</td>
+                        <td className="px-4 py-2 border text-center">{Object.keys(db[0]["LeBron"][0])[0]}</td>
+                        <td className="px-4 py-2 border text-center">{db[0]["LeBron"][0]["Tylenol"]}</td>
+                        <td className="px-4 py-2 border text-center">
+                            <button onClick={incrementDosage} className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110">
+                                +
+                            </button>
+                            <button onClick={decrementDosage} className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110">
+                                -
+                            </button>
+                            <button className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110">
+                                Edit
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
