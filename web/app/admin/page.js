43fa0ca76b6cdb1f31d68f28bafa5467eeff17db
Felix Zheng <zfelix081222@gmail.com>
2026-01-10 21:50:46 -0500

    abc

--- web/app/admin/page.js
+++ web/app/admin/page.js
@@ -1,97 +1,9 @@
 "use client";
-import { useState, useRef } from "react";
-import User from "../admin/user.js"
-
-// BLE service UUIDs
-const SERVICE = "12345678-1280-1280-1280-676767abcdef";
-const CHARACTERISTIC = "87654321-1280-1280-1280-abcdef676767";
-const CMD_CHARACTERISTIC = "12345678-1280-1280-1280-abcdefabcdef";
+import { useRef } from 'react';
+import User from "../admin/user.js";
 
 export default function Admin() {
-    const [db, setDb] = useState([
-    { LeBron: [{ Tylenol: 0 }, { Vivace: 3 }] },
-    { LeTwo: [{ Tylenol: 4 }, { Vivace: 2 }] },
-    ]);
-    
-    const cmdCharacteristicRef = useRef(null);
-
-    async function connectToDevice() {
-        try {
-            console.log("Requesting Bluetooth Device...");
-
-            const device = await navigator.bluetooth.requestDevice({
-                filters: [{ services: [SERVICE] }],
-            });
-            const server = await device.gatt.connect();
-            const service = await server.getPrimaryService(SERVICE);
-            const characteristic = await service.getCharacteristic(CHARACTERISTIC);
-            const cmdCharacteristic = await service.getCharacteristic(CMD_CHARACTERISTIC);
-            cmdCharacteristicRef.current = cmdCharacteristic;
-
-            // Initial read
-            const initial = await characteristic.readValue();
-            const initialCounter = initial.getUint32(0, true);
-            setDb((prev) => {
-                const copy = structuredClone(prev);
-                copy[0].LeBron[0].Tylenol = initialCounter;
-                return copy;
-            });
-
-            console.log(`Initial Dosage: ${initialCounter}`);
-
-            // Subsequent notifications
-            await characteristic.startNotifications();
-            characteristic.addEventListener("characteristicvaluechanged", (event) => {
-                const counter = event.target.value.getUint32(0, true);
-                console.log(`Dosage Updated: ${counter}`);
-
-                setDb((prev) => {
-                    const copy = structuredClone(prev);
-                    copy[0].LeBron[0].Tylenol = counter;
-                    return copy;
-                });
-            });
-        } catch (error) {
-            console.log("Oops... " + error);
-        }
-    }
-
-    const incrementDosage = async () => {
-        setDb(prev => {
-            const copy = structuredClone(prev)
-            copy[0].LeBron[0].Tylenol += 1
-            return copy
-        } 
-        );
-
-        if (cmdCharacteristicRef.current) {
-            try {
-                await cmdCharacteristicRef.current.writeValue(new Uint8Array([2]));
-                console.log("Sent increment command to device");
-            } catch (error) {
-                console.error("Error sending command to device: ", error);
-            }
-        }
-    }
-    const decrementDosage = async () => {
-        setDb(prev => {
-            const copy = structuredClone(prev)
-            copy[0].LeBron[0].Tylenol -= copy[0].LeBron[0].Tylenol > 0 ? 1 : 0;
-            return copy
-        }
-        );
-
-        if (cmdCharacteristicRef.current) {
-            try {
-                await cmdCharacteristicRef.current.writeValue(new Uint8Array([1]));
-                console.log("Sent decrement command to device");
-            } catch (error) {
-                console.error("Error sending command to device: ", error);
-            }
-        }
-    }
-
-      const lebron = {
+  const lebron = {
     username: "LeBron",
     drugs: [
       ["Tylenol", 0, 1, "pills"],
@@ -105,25 +17,20 @@ export default function Admin() {
       ["Advil", 0, 2, "pills"],
       ["Omega 3", 0, 1, "capsules"]
     ]
-    }
+  }
 
-    return (
-        <div className="p-10">
-            <title>
-                Admin Page
-            </title>
-            <h1 className="flex justify-left text-3xl">
-                Add a prescription to get started.
-            </h1>
-            <div className="m-4">
-                <button onClick={connectToDevice} className="px-4 py-2 bg-green-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-105">
-                    Connect to Prescription
-                </button>
-            </div>
-             <div>
-                   <User userNameExternal={lebron.username} drugsExternal={lebron.drugs} />
-                   <User userNameExternal={drake.username} drugsExternal={drake.drugs} />
-             </div>
-        </div>
-    )
+  return (
+    <div className="p-10">
+      <title>
+      Admin Page
+      </title>
+      <h1 className="flex justify-left text-3xl">
+      Add a prescription to get started.
+      </h1>
+      <div>
+        <User userNameExternal={lebron.username} drugsExternal={lebron.drugs} />
+        <User userNameExternal={drake.username} drugsExternal={drake.drugs} />
+      </div>
+    </div>
+  )
 }
--- web/app/admin/user.js
+++ web/app/admin/user.js
@@ -1,60 +1,6 @@
 "use client";
-// import { useState } from "react";
-
-// // BLE service UUIDs
-// const SERVICE = "12345678-1280-1280-1280-676767deltah";
-// const CHARACTERISTIC = "87654321-1280-1280-1280-deltah676767";
-
-// export default function Admin() {
-//     const [db, setDb] = useState([
-//     { LeBron: [{ Tylenol: 1 }, { Vivace: 3 }] },
-//     { LeTwo: [{ Tylenol: 4 }, { Vivace: 2 }] },
-//     ]);
-
-//     const decrementDosage = () => {
-//         setDb(prev => {
-//             const copy = structuredClone(prev)
-//             copy[0].LeBron[0].Tylenol -= 1
-//             return copy
-//         }
-//         );
-//     }
-
-//     return (
-//         <div className="p-10">
-//             <title>
-//                 Admin Page
-//             </title>
-//             <h1 className="flex justify-left text-3xl">
-//                 Add a prescription to get started.
-//             </h1>
-//             <table className="w-9/12 border border-gray-300 m-10 mx-auto">
-//                 <thead>
-//                     <tr>
-//                         <th className="px-4 py-2 border">User</th>
-//                         <th className="px-4 py-2 border">Medication</th>
-//                         <th className="px-4 py-2 border">Dosage</th>
-//                         <th className="px-4 py-2 border">Edit</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     <tr>
-//                         <td className="px-4 py-2 border text-center">{Object.keys(db[0])[0]}</td>
-//                         <td className="px-4 py-2 border text-center">{Object.keys(db[0]["LeBron"][0])[0]}</td>
-//                         <td className="px-4 py-2 border text-center">{db[0]["LeBron"][0]["Tylenol"]}</td>
-//                         <td className="px-4 py-2 border text-center">
-                            
-//                         </td>
-//                     </tr>
-//                 </tbody>
-//             </table>
-//         </div>
-//     )
-// }
-
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 process.env.NEXT_PUBLIC_API_URL = "http://localhost:4000" //Remove please
-
 
 export default function User(props) {
   
@@ -73,32 +19,31 @@ export default function User(props) {
   // Drug name, taken today, daily dose, dose unit
   const [username, setUsername] = useState(props.userNameExternal)
   const [drugs, setDrugs] = useState(props.drugsExternal);
-    
-
-  function incrementHandleTrack(i) {
-  setDrugs(prev => {
-    const updated = prev.map((drug, index) => index === i ? [...drug] : drug);
-    // const updated = [...prev];
-    updated[i][1] = updated[i][1] + 1; // increment takenToday
-    return updated;
-  });
-}
-
-  function decrementHandleTrack(i) {
-  setDrugs(prev => {
-    const updated = prev.map((drug, index) => index === i ? [...drug] : drug);
-    // const updated = [...prev];
-    updated[i][1] = updated[i][1] - 1; // increment takenToday
-    return updated;
-  });
-}
-
+  
+  async function incrementHandleTrack(i) {
+    setDrugs(prev => {
+      const updated = prev.map((drug, index) => index === i ? [...drug] : drug);
+      // const updated = [...prev];
+      updated[i][1]++; // increment takenToday
+      return updated;
+    });
+  }
+  
+  async function decrementHandleTrack(i) {
+    setDrugs(prev => {
+      const updated = prev.map((drug, index) => index === i ? [...drug] : drug);
+      // const updated = [...prev];
+      updated[i][1] -= updated[i][1] > 0 ? 1 : 0; // decrement takenToday
+      return updated;
+    });
+  }
+  
   return (
     <div className="flex flex-col align-middle">
       <h1 className="flex justify-center text-5xl p-8"> 
-        {username}
+      {username}
       </h1>
-
+    
     <table className="w-9/12 border border-gray-300 mx-auto">
       <thead>
         <tr>
@@ -108,31 +53,31 @@ export default function User(props) {
           <th className="px-4 py-2 border">Count</th>
         </tr>
       </thead>
-
-      <tbody>
-        {drugs.map((drug, index) => (
-          <tr key={index}>
-            <td className="px-4 py-2 border">{drug[0]}</td>
-            <td className="px-4 py-2 border">{drug[2]}</td>
-            <td className="px-4 py-2 border">{`${drug[1]} / ${drug[2]} ${drug[3]}`}</td>
-
-            <td className="px-4 py-2 border flex justify-center">
-              <button onClick={() => incrementHandleTrack(index)} className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110 size-10">
-                +
-              </button>
-              <button onClick={() => decrementHandleTrack(index)} className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110 size-10">
-                -
-              </button>
-              <button className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110">
-                Edit
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-      
+    
+    <tbody>
+      {drugs.map((drug, index) => (
+        <tr key={index}>
+        <td className="px-4 py-2 border">{drug[0]}</td>
+        <td className="px-4 py-2 border">{drug[2]}</td>
+        <td className="px-4 py-2 border">{`${drug[1]} / ${drug[2]} ${drug[3]}`}</td>
+        
+        <td className="px-4 py-2 border flex justify-center">
+          <button onClick={() => incrementHandleTrack(index)} className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110 size-10">
+            +
+          </button>
+          <button onClick={() => decrementHandleTrack(index)} className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110 size-10">
+            -
+          </button>
+          <button className="px-3 py-1 bg-blue-500 text-white rounded m-2 transition duration-300 ease-in-out hover:scale-110">
+            Edit
+          </button>
+        </td>
+        </tr>
+      ))}
+    </tbody>
+    
     </table>
-    <button className="px-3 py-1 bg-blue-500 text-white rounded mx-auto my-5 transition duration-300 ease-in-out hover:scale-110 length-50" onClick={() => ping('updatedrugs', {username: "Bronny", drugs: drugs})}>Add Drugs</button>
+      <button className="px-3 py-1 bg-blue-500 text-white rounded mx-auto my-5 transition duration-300 ease-in-out hover:scale-110 length-50" onClick={() => ping('updatedrugs', {username: "Bronny", drugs: drugs})}>Add Drugs</button>
     </div>
   )
 }
